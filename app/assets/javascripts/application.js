e8c9dbe826a47973a929af7c06df81960bd70b75
sneh <snehp92@gmail.com>
2014-10-16 16:01:31 -0400

    Final

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,83 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery.infinitescroll
+//= require jquery_ujs
+//= require twitter/bootstrap
+//= require social-share-button
+//= require turbolinks
+//= require_tree .
+$(document).ready(function(){
+    $('#quantity').focusout(calculate);
+    $('#new').hide();
+    
+});
+function calculate(e)
+{
+   ans = $('#quantity').val() * 100;
+   $('#cents').val(ans )
+   $('#pay').val($('#quantity').val())
+}
+ $(document).ready(function () {
+  //called when key is pressed in textbox
+  $("#quantity").keypress(function (e) {
+     //if the letter is not digit then display error and don't type anything
+     if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+        //display error message
+        $("#errmsg").html("Digits Only").show().fadeOut("slow");
+               return false;
+    }
+    else
+    {
+       $('#new').show();
+    }
+   
+   });
+});
+
+$(document).ready(function () {
+
+    $(".player").mb_YTPlayer();
+
+});
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
+});
+var activeEl = 2;
+$(function() {
+    var items = $('.btn-nav');
+    $( items[activeEl] ).addClass('active');
+    $( ".btn-nav" ).click(function() {
+        $( items[activeEl] ).removeClass('active');
+        $( this ).addClass('active');
+        activeEl = $( ".btn-nav" ).index( this );
+    });
+});
+//= require jquery.infinitescroll
