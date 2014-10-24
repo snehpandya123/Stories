e7f611a9d31d2acec687d9d9013724b6a2bb0fd3
sneh <snehp92@gmail.com>
2014-10-24 02:05:13 -0400

    main_page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,21 @@
 //= require social-share-button
 //= require turbolinks
 //= require_tree .
+$(function(){
+$('a[title]').tooltip();
+});
 
-
+$(document).ready(function() {
+    $(document).mousemove(function(event) {
+        TweenLite.to($(".register"), 
+        .5, {
+            css: {
+                backgroundPosition: "" + parseInt(event.pageX / 8) + "px " + parseInt(event.pageY / '12') + "px, " + parseInt(event.pageX / '15') + "px " + parseInt(event.pageY / '15') + "px, " + parseInt(event.pageX / '30') + "px " + parseInt(event.pageY / '30') + "px",
+                "background-position": parseInt(event.pageX / 8) + "px " + parseInt(event.pageY / 12) + "px, " + parseInt(event.pageX / 15) + "px " + parseInt(event.pageY / 15) + "px, " + parseInt(event.pageX / 30) + "px " + parseInt(event.pageY / 30) + "px"
+            }
+        })
+    })
+})
 
 
 $(document).ready(function(){
